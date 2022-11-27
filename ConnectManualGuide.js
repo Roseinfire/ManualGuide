75295316233e17a43f2acea42b5a8a67adcd9a35
Roseinfire <14box14@gmail.com>
2022-11-27 08:29:24 +0300

    Update ConnectManualGuide.js

--- ConnectManualGuide.js
+++ ConnectManualGuide.js
@@ -5,9 +5,9 @@ window.addEventListener("load", function() {
 fetch('https://roseinfire.github.io/ManualGuide/ManualGuide.json')
   .then(response => response.text())
   .then(text => establishManualGuide(text))
- 
+  
   function establishManualGuide(text) {
-    localStorage.setItem("ManualGuide/", document.getElementById("content").innerHTML)
+    window.content =  document.getElementById("content").innerHTML
     document.write(text)
     }
 })
