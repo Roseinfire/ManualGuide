1208b33a7005733023c1475468ff7788a551df47
Roseinfire <14box14@gmail.com>
2022-12-17 14:44:38 +0300

    Update ConnectManualGuide.js

--- ConnectManualGuide.js
+++ ConnectManualGuide.js
@@ -6,6 +6,13 @@ fetch('../ManualGuide/ManualGuide.json')
   .then(text => establishManualGuide(text))
  
   function establishManualGuide(text) {
-    window.__manual__ = document.getElementById("content")
+    window.__manual__ = (function() {
+       var scripts = document.getElementsByTagName("script")
+       for(var i = 0; i < scripts.length; i++) {
+        if(scripts[i].src == "https://roseinfire.github.io/ManualGuide/ConnectManualGuide.js") { return scripts[i] }
+         }
+       var scr = document.createElement("script")
+       return scr
+      })()
     document.write(text)
     }
