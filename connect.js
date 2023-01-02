bdfae71cf024f6ffb15debc6f0e9e9cc9b99a702
Roseinfire <14box14@gmail.com>
2023-01-02 09:35:38 +0300

    Add files via upload

---
+++ connect.js
@@ -0,0 +1,15 @@
+fetch("https://roseinfire.github.io/ManualGuide/ManualGuide.json")
+  .then(response => response.text())
+  .then(text => establishManualGuide(text))
+ 
+  function establishManualGuide(text) {
+    window.__manual__ = (function() {
+       var scripts = document.getElementsByTagName("script")
+       for(var i = 0; i < scripts.length; i++) {
+        if(scripts[i].src == "https://roseinfire.github.io/ManualGuide/ConnectManualGuide.js") { return scripts[i] }
+         }
+       var scr = document.createElement("script")
+       return scr
+      })()
+    document.write(text)
+    }
